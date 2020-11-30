4d723796d20655d420ed441d3b1f309c331c6c8f
Aidy <microsoft@aidanjenkins.co.uk>
2020-11-30 20:17:12 +0000

    Adjusted to use Parzec/BigNumber.js Implemented expression parsing with custom inputs

--- src/Decimalize.ts
+++ src/Decimalize.ts
@@ -1,110 +1,85 @@
-import { Big, BigSource } from "big.js";
+import { BigNumber } from "BigNumber.js";
+import * as pz from "parzec";
+
+enum ExprToken { 
+    Input,
+    Number, OpenParen, CloseParen, Plus, Minus, Multiply, Divide, Whitespace, 
+    EOF 
+}
+
+const lexer = new pz.Lexer(
+    [/\{\d\}/, ExprToken.Input],
+    [/-?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?/, ExprToken.Number],
+    [/\(/, ExprToken.OpenParen],
+    [/\)/, ExprToken.CloseParen],
+    [/\+/, ExprToken.Plus],
+    [/-/, ExprToken.Minus],
+    [/\*/, ExprToken.Multiply],
+    [/\//, ExprToken.Divide],
+    [/[\t\n\r ]+/, ExprToken.Whitespace]);
+
+const optws = pz.terminal(ExprToken.Whitespace, "<whitespace>").optionalRef();
+let inputFromArg: (inputIndex: string) => BigNumber;
+const input = pz.terminal(ExprToken.Input, "<input>").map(t => inputFromArg(t.text)).followedBy(optws);
+const number = pz.terminal(ExprToken.Number, "<number>").map(token => new BigNumber(token.text)).followedBy(optws);
+const openParen = pz.terminal(ExprToken.OpenParen, "(").followedBy(optws);
+const closeParen = pz.terminal(ExprToken.CloseParen, ")").followedBy(optws);
+const plus = pz.terminal(ExprToken.Plus, "+").followedBy(optws);
+const minus = pz.terminal(ExprToken.Minus, "-").followedBy(optws);
+const multiply = pz.terminal(ExprToken.Multiply, "*").followedBy(optws);
+const divide = pz.terminal(ExprToken.Divide, "/").followedBy(optws);
+const eof = pz.terminal(ExprToken.EOF, "<end of input>");
+
+const addop = pz.operators(
+    [plus, (a: BigNumber, b: BigNumber) => a.plus(b)],
+    [minus, (a: BigNumber, b: BigNumber) => a.minus(b)]);
+
+const mulop = pz.operators(
+    [multiply, (a: BigNumber, b: BigNumber) => a.times(b)],
+    [divide, (a: BigNumber, b: BigNumber) => a.div(b)]);
+
+const term = new pz.Ref<pz.Parser<BigNumber, pz.Token<ExprToken>>>();
+const expr = pz.forwardRef(term).chainOneOrMore(addop);
+const factor = expr.bracketedBy(openParen, closeParen).or(number).or(input);
+term.target = factor.chainOneOrMore(mulop);
+
+const rootExpr = optws.seq(expr).followedBy(eof);
+
+const numberFromArg = (arg: string) => parseInt(arg.substring(1, arg.length - 1)); //{123} -> 123
+
 
 export const dec = (strings: TemplateStringsArray, ...args: ExpressionElement[]) => {
-    let elements = Decimalize.combineElements(strings, ...args);
+    let argIndices = args.map((_, idx) => `{${idx}}`);
+    
+    let elements = Decimalize.combineElements(strings, ...argIndices);
 
-    let expression = Decimalize.parseExpression(elements);
-
-    let result = Decimalize.evaluate(expression);
+    let result = Decimalize.evaluate(elements.join(''), args);
 
     return result;
 };
 
-type ExpressionElement = number | Big | string | bigint;
-type ExpressionParameter = { parameter: ExpressionElement };
-type ExpressionInput = (string | ExpressionParameter);
-type Expression = ExpressionInput[];
+type ExpressionElement = number | BigNumber | string | bigint;
 
-export module Decimalize {
+module Decimalize {
 
-    export function combineElements(strings: TemplateStringsArray | string[], ...elements: ExpressionElement[]) {
-        let combined = [] as ExpressionInput[];
+    export function combineElements(strings: TemplateStringsArray | string[], ...elements: string[]) {
+        let combined = [] as string[];
 
         for (let i = 0; i < strings.length; ++i) {
             combined.push(strings[i]);
             if (i < elements.length) {
-                combined.push({ parameter: elements[i] });
+                combined.push(elements[i]);
             }
         }
 
         return combined;
     }
 
-    export function parseExpression(elements: ExpressionInput[]) {
-        return elements.flatMap(element => {
-            if (typeof element === "string")
-                return element.split(' ') as ExpressionInput[];
+    export function evaluate(expression: string, inputs: ExpressionElement[]) {
+        
+        inputFromArg = arg => new BigNumber(inputs[numberFromArg(arg)] as number | BigNumber | string);
 
-            return element;
-        });
-    }
-
-    export const operands = [
-        ['(', ')'],
-        ['*', '/'],
-        ['+', '-']
-    ];
-
-    export function evaluate(expression: Expression) {
-        for (let precedence of operands) {
-
-            let left: ExpressionInput;
-            let operand: string;
-
-            let next = [] as Expression;
-
-            while(expression.some(item => precedence.includes(item as string))) {
-
-                for (let item of expression) {
-                    if (typeof item === "string") {
-                        if (item.trim() === '') continue;
-
-                        if (precedence.includes(item)) {
-                            operand = item;
-                            continue;
-                        }
-
-                        let isOperand = operands.some(precedence => precedence.includes(item as string));
-                        if(isOperand) {
-                            next.push(item);
-                            continue;
-                        }
-                    }
-
-                    if(operand != null) {
-                        next.push({ parameter: resolve(operand, left, item)});
-                    }
-
-                    left = item;
-                }
-
-                expression = next;
-            }
-        }
-
-        return (expression.pop() as ExpressionParameter)?.parameter as Big;
-    }
-
-    export function resolve(operand: string, left: ExpressionInput, right: ExpressionInput) {
-        let leftValue = typeof left === "object" ? Big(left.parameter as any as BigSource) : Big(left);
-        let rightValue = typeof right === "object" ? Big(right.parameter as any as BigSource) : Big(right);
-
-        try {
-            switch(operand) {
-                case '*': 
-                    return leftValue.mul(rightValue);
-                case '/':
-                    return leftValue.div(rightValue);
-                case '+':
-                    return leftValue.add(rightValue);
-                case '-':
-                    return leftValue.minus(rightValue);
-                default:
-                    return NaN;
-            }
-        }
-        catch(err) {
-            return NaN;
-        }
+        return pz.parse(rootExpr, pz.lexerInput<ExprToken>(expression, lexer, 
+            new pz.Token(ExprToken.EOF, "<end of input>")));
     }
 }
